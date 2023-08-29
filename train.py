163a57f9ec467d9c85ea7c8ca03ecafa3f1725c5
ProfessorChaos <paul.lintilhac@gmail.com>
2023-08-28 20:01:05 -0400

    increased accuracy stopping threshold

--- train.py
+++ train.py
@@ -279,7 +279,6 @@ if wandb_log and master_process:
 
 # training loop
 X, Y = get_batch('train') # fetch the very first batch
-print("here")
 t0 = time.time()
 local_iter_num = 0 # number of iterations in the lifetime of this process
 raw_model = model.module if ddp else model # unwrap DDP container if needed
